94fb6a1dc57f3079402b45bf508bcc87db05ccb3
Christos Lordopoulos <Christos.Lordopoulos@sp-con.de>
2025-05-26 15:36:03 +0300

    test

--- bet-exchange/vite.config.ts
+++ bet-exchange/vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    // allowedHosts: ['frontend', 'playwright-test', 'backend'],
+    allowedHosts: ['frontend', 'playwright-test', 'backend'],
   },
 })
