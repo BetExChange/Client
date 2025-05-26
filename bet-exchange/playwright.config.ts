da2e397c380a4fbede010b662c43721f4903b9b8
Christos Lordopoulos <Christos.Lordopoulos@sp-con.de>
2025-05-26 16:06:19 +0300

    test

--- bet-exchange/playwright.config.ts
+++ bet-exchange/playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.CI ? 'http://backend:8080' : 'http://localhost:8080',
     screenshot: 'only-on-failure',
-    navigationTimeout: 30 * 1000,
+    navigationTimeout: 1000 * 1000,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
