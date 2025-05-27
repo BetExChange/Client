c39b010259769ee15b11c782a016b0740b48693a
Christos Lordopoulos <Christos.Lordopoulos@sp-con.de>
2025-05-27 10:40:02 +0300

    test

--- bet-exchange/playwright.config.ts
+++ bet-exchange/playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
   },
   retries: 2,
 
