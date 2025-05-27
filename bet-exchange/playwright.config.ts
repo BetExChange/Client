c28e7d161f4c33f2f6fa5727cc3add7edfca20f0
Christos Lordopoulos <Christos.Lordopoulos@sp-con.de>
2025-05-27 14:42:37 +0300

    test

--- bet-exchange/playwright.config.ts
+++ bet-exchange/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.VITE_FE_BASE_URL || 'http://localhost:5173',
+    // baseURL: process.env.VITE_FE_BASE_URL || 'http://localhost:5173',
     screenshot: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
